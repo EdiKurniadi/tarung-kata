2e426f6418099cedeec023b0d10dc318af725e27
Edi Kurniadi <82230087+EdiKurniadi@users.noreply.github.com>
2022-11-13 22:36:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ let initializeWord = (dataFromEnemy) => {
 	}
 
 	for(let i = 0 ; i<5 ; i++) {
-		allWords[allWords.length - 1].children[i].style.display = 'inline';
+		allWords[allWords.length - 1].children[i].style.display = 'inline-block';
 		allWords[allWords.length - 1].children[i].setAttribute('class','box-letter')
 		allWords[allWords.length - 1].children[i].innerHTML = beginWordArray[i].toUpperCase();
 	}
